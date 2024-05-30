2a5a9a4412ea58f4a4418bdaa1c6719a231cc822
Yahav Corcos <ycorcos26@gmail.com>
2024-05-30 16:08:51 -0400

    Update commit

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <h1>Wordle</h1>
+        <h1>Wordle (Clone)</h1>
       </nav>
       <AppContext.Provider
         value={{
